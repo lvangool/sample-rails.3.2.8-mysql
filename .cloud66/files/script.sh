8b28d5d1ab3120944578d813608aeff54f907636
Luvic van Gool <vic@thecloudblocks.com>
2013-01-17 15:18:30 +0000

    extra packages

---
+++ .cloud66/files/script.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Detects linux distro
+
+OS=`uname -s`
+
+if [ "${OS}" = "Linux" ] ; then
+	if [ -f /etc/redhat-release ] ; then
+		DIST='RedHat/CentOS'
+	elif [ -f /etc/SuSE-release ] ; then
+		DIST="SuSE"
+	elif [ -f /etc/mandrake-release ] ; then
+		DIST='Mandrake'
+	elif [ -f /etc/lsb-release ] ; then
+		DIST="Ubuntu"
+	elif [ -f /etc/debian_version ] ; then
+		DIST="Debian"
+	elif [ -f /etc/UnitedLinux-release ] ; then
+		DIST="UnitedLinux"
+	elif [ -f /etc/system-release ] ; then
+		DIST="Amazon"
+	fi
+fi
+
+echo ${DIST} > ~/distro.txt
